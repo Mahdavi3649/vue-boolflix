ce72fc5bc9962d8d2f30f538227a953f668d6305
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-09 11:48:17 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="d-flex">
+        <img src="@/assets/img/logo2.jpg" />
+      </div>
+
+      <form class="d-flex">
+        <input
+          class="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <button
+          class="btn btn-outline-danger d-flex align-items-center"
+          type="submit"
+        >
+          <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+          Search
+        </button>
+      </form>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
