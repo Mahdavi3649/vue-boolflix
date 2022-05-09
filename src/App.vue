a78c34aa173b8a3e682e91e6a969963c2cb811d8
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-09 16:53:58 +0200

    Add serieTV api

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,37 @@
     <main>
       <div class="container">
         <div class="row">
-          <ul>
-            <li v-for="(movie, index) in movies" :key="movie.id">
-              <img
-                :src="'http://image.tmdb.org/t/p/w200/' + movie.poster_path"
-                alt=""
-              />
-              <h3>{{ movie.title }}</h3>
-              <p>{{ movie.original_title }}</p>
-              <img
-                :src="'https://flagcdn.com/w20/' + flags(index) + '.png'"
-                alt=""
-              />
-              <p>{{ movie.vote_average }}</p>
-            </li>
-          </ul>
+          <div class="col" v-for="(movie, index) in movies" :key="movie.id">
+            <img
+              :src="'http://image.tmdb.org/t/p/w200/' + movie.poster_path"
+              alt=""
+            />
+
+            <h3>{{ movie.title }}</h3>
+            <p>{{ movie.original_title }}</p>
+            <img
+              :src="'https://flagcdn.com/w20/' + flags(index) + '.png'"
+              alt=""
+            />
+            <p>{{ movie.vote_average }}</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col" v-for="(serie, index) in series" :key="serie.id">
+            <img
+              :src="'http://image.tmdb.org/t/p/w200/' + serie.poster_path"
+              alt=""
+            />
+
+            <h3>{{ serie.name }}</h3>
+            <p>{{ serie.original_name }}</p>
+            <img
+              :src="'https://flagcdn.com/w20/' + flags(index) + '.png'"
+              alt=""
+            />
+            <p>{{ serie.vote_average }}</p>
+          </div>
         </div>
       </div>
     </main>
@@ -57,29 +73,31 @@ export default {
     return {
       searchText: "",
       movies: null,
-      API_URL:
-        "https://api.themoviedb.org/3/search/movie?api_key=7559f4c99fde3dbacdffc9766ef20e18&language=it-IT&page=1&include_adult=false&query=funny games",
+      series: null,
     };
   },
   methods: {
     searchMovie() {
-      axios
-        .get(this.API_URL)
-        .then((response) => {
-          console.log(response);
-          this.movies = response.data.results;
+      let APImovies = axios.get(
+        "https://api.themoviedb.org/3/search/movie?api_key=7559f4c99fde3dbacdffc9766ef20e18&language=it-IT&page=1&include_adult=false&query=funny games"
+      );
+      let APIseries = axios.get(
+        "https://api.themoviedb.org/3/search/tv?api_key=7559f4c99fde3dbacdffc9766ef20e18&language=en-US&page=1&include_adult=false&query=black mirror"
+      );
+      axios.all([APImovies, APIseries]).then(
+        axios.spread((...responses) => {
+          this.movies = responses[0].data.results;
+          this.series = responses[1].data.results;
+          this.searchText = "";
         })
-        .catch((error) => {
-          console.log(error);
-          //this.error = `OPS! ${error}`;
-        });
+      );
     },
 
     flags(index) {
-      if (this.movies[index].original_language[index] === "en") {
-        this.movies[index].original_language[index] = "us";
-      } else if (this.movies[index].original_language[index] === "de") {
-        this.movies[index].original_language[index] = "de";
+      if (this.movies[index].original_language === "en") {
+        this.movies[index].original_language = "us";
+      } else if (this.movies[index].original_language === "de") {
+        this.movies[index].original_language = "de";
       }
       return this.movies[index].original_language;
     },
